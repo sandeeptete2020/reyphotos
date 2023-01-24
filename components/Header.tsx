bec73dd5ca57a4e1da6a6bcb91113aa6365b1d09
sandeeptete23 <119023082+sandeeptete23@users.noreply.github.com>
2023-01-25 01:31:51 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
         </h1>
       </Link>
       <a
-        href="https://vercel.com/templates/next.js/ai-photo-restorer"
+        href="https://vercel.com"
         target="_blank"
         rel="noreferrer"
       >
