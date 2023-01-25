65fd2c8409969da1cc297ff700ca6ab8c2b5050b
sandeeptete23 <119023082+sandeeptete23@users.noreply.github.com>
2023-01-25 10:36:56 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,8 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-        <script src="https://www.googletagmanager.com/gtag/js?id=G-BKWNKDQJQH"
-        strategy="afterInteractive"
-      />
-      <script id="google-analytics" strategy="afterInteractive">
-        {'
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
+       <Analytics trackingId="GA-XXXXXXXX-X" />
 
-          gtag('config', 'G-BKWNKDQJQH');
-        '}
-      </script>
       <Component {...pageProps} />
       <Analytics />
     </>
