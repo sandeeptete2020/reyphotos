97ed045c55af77ade9628cfce282e011fa67c866
sandeeptete23 <119023082+sandeeptete23@users.noreply.github.com>
2023-01-25 11:16:55 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,25 @@
 import { Analytics } from "@vercel/analytics/react";
+import Script from 'next/script';
+
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BKWNKDQJQH"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {'
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
 
+          gtag('config', 'G-BKWNKDQJQH');
+        '}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
