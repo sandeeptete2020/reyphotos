f56534e5d288596516ee4a926e1f989a4aaf14cc
sandeeptete23 <119023082+sandeeptete23@users.noreply.github.com>
2023-01-25 09:55:08 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
-        Powered by{" "}
+        {/* Powered by{" "}
         <a
           href="https://replicate.com/"
           target="_blank"
@@ -21,7 +21,7 @@ export default function Footer() {
           className="font-bold hover:underline transition underline-offset-2"
         >
           Vercel.
-        </a>
+        </a>*/}
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
