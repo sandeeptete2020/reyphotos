a62b3d92becc0c2d6b601cca097b844ce5925c60
sandeeptete2020 <119023082+sandeeptete2020@users.noreply.github.com>
2023-01-27 19:41:05 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,10 +18,9 @@ const Home: NextPage = () => {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
         
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
-          Preserving history{" "}
+          Preserving history using AI,{" "}
           <span className="relative whitespace-nowrap text-[#3290EE]">
             <SquigglyLines />
-            <span className="relative">using AI</span>,
           </span>{" "}
           one pixel at a time.
         </h1>
