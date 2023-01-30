00d48dc9146c5401f4b6377ec39eafa3e33a386b
sandeeptete2020 <119023082+sandeeptete2020@users.noreply.github.com>
2023-01-30 11:51:59 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,9 +16,7 @@ export default function Header() {
          ReyGrow
         </h1>
       </Link>
-      <Head>
-        <title>AI-Powered Photo Restoration - Bring Faded Memories Back to Life</title>
-      </Head>
+      
     </header>
   );
 }
